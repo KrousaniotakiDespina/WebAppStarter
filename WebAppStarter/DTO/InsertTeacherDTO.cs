57262874018cd78f375559a15d20e4324b45dec6
KrousaniotakiDespina <despoinakrr@gmail.com>
2026-07-18 23:38:43 +0300

    feat: add dto for insert teacher

---
+++ WebAppStarter/DTO/InsertTeacherDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppStarter.DTO
+{
+    public record InsertTeacherDTO
+        (
+            [property: Required(ErrorMessage = "Firstname is required.")]
+            [property: MinLength(1, ErrorMessage = "Firstname must not be empty.")]
+            string? Firstname,
+
+            [property: Required(ErrorMessage = "Lastname is required.")]
+            [property: MinLength(1, ErrorMessage = "Lastname must not be empty.")]
+            string? Lastname,
+
+            int SelectedCityId
+        )
+    {
+        public InsertTeacherDTO() : this(default, default, default) { }
+    }
+}
---
+++ WebAppStarter/DTO/TeacherReadOnlyDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace WebAppStarter.DTO
+{
+    public record TeacherReadOnlyDTO(int Id, string? Firstname, string? Lastname)
+    {
+        public TeacherReadOnlyDTO() : this(default, default, default) { }
+    }
+}
